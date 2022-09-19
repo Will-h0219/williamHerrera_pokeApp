afdae16daf879171647467fa9e2e2ed54980ccb2
Will-h0219 <wherrera0219@gmail.com>
2022-09-19 02:49:50 -0500

    SearchBarComponent tests added

--- src/app/modules/pokemon/components/search-bar/search-bar.component.spec.ts
+++ src/app/modules/pokemon/components/search-bar/search-bar.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { detailPokemonMock } from 'src/app/utils/mocks/pikachu.mock';
+import { PokemonService } from '../../../../data/services/pokemon.service';
 
 import { SearchBarComponent } from './search-bar.component';
 
@@ -6,9 +11,16 @@ describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
 
+  const pokemon = detailPokemonMock;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      declarations: [ SearchBarComponent ],
+      providers: [ PokemonService ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +33,55 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('El valor buscado debe ser valido', () => {
+    const control = component.searchInput;
+    control?.setValue('pikachu');
+    expect(control?.valid).toBeTruthy();
+    
+    control?.setValue('pika_+');
+    expect(control?.valid).toBeFalsy();
+  });
+
+  it('Debe emitir reset si no hay un valor', () => {
+    const elem = fixture.debugElement.query(By.css('.form__field')).nativeElement;
+    const control = component.searchInput;
+
+    control?.setValue('pikachu');
+    fixture.detectChanges();
+    expect(elem.value).toBe('pikachu');
+    
+    control?.setValue('');
+    fixture.detectChanges();
+    expect(elem.value).toBe('');
+
+    spyOn(component.onSearch, 'emit');
+    
+    const ev = new KeyboardEvent("keyup", {
+      key: 'Enter'
+    });
+    elem.dispatchEvent(ev);
+    fixture.detectChanges();
+
+    expect(component.onSearch.emit).toHaveBeenCalledWith('reset');
+  });
+
+  it('Debe limpiar en el click y emitir reset', () => {
+    const elem = fixture.debugElement.query(By.css('.form__field')).nativeElement;
+    const btn = fixture.debugElement.query(By.css('.clear-btn'));
+    const control = component.searchInput;
+
+    spyOn(component.onSearch, 'emit');
+
+    control?.setValue('pikachu');
+    fixture.detectChanges();
+    expect(elem.value).toBe('pikachu');
+    
+    btn.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(elem.value).toBe('');
+    expect(component.onSearch.emit).toHaveBeenCalledWith('reset');
   });
 });
